defd246d48f0079b9fc5f9317754d7427eccb053
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-05 16:26:26 +0530

    Modified the logic

--- ConverToListAndTuple.py
+++ ConverToListAndTuple.py
@@ -4,6 +4,8 @@
 numbers = input("Proved the sequence of comma seperated numbers: ")
 
 l = list(numbers)
+#list can also be written as follow
+#l = numbers.split(",")
 t = tuple(numbers)
 
 print("The sequence of the numbers you provided are converted to list as: ",l)
