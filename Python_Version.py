bd70d29f7aa4eb6085c2960048af114c67977b94
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-05 14:25:54 +0530

    Program to find which python version you are using

---
+++ Python_Version.py
@@ -0,0 +1,7 @@
+#Program to find which python version you are using
+
+#import sys
+#This module provides access to some variables used or maintained by the interpreter 
+
+import sys
+print("Python Version is",sys.version,"and version info is:",sys.version_info)
