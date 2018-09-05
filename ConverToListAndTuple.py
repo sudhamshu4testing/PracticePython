632f36169f07e30edb73e335f91eafebd500fab6
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-05 16:19:25 +0530

    Program to convert the user entered values to List and Tuple

---
+++ ConverToListAndTuple.py
@@ -0,0 +1,10 @@
+#Program to convert the user entered values to List and Tuple
+
+#Ask user to provide a sequence of comma seperated numbers
+numbers = input("Proved the sequence of comma seperated numbers: ")
+
+l = list(numbers)
+t = tuple(numbers)
+
+print("The sequence of the numbers you provided are converted to list as: ",l)
+print("The sequence of the numbers you provided are converted to tuple as: ",t)
