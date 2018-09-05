331c06da96e4083c55fdc41c951391932dab372e
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-05 16:14:20 +0530

    Program to reverse the strings

---
+++ Reverse_Strings.py
@@ -0,0 +1,8 @@
+#Program to reverse the strings
+
+#Ask user to provide first_name and last_name
+first_name = raw_input("Please provide your first name: ")
+last_name = raw_input("Please provide your last name: ")
+
+#Printing the users first_name and last_name in reverse order with a space
+print("Hello " + last_name + " " + first_name)
