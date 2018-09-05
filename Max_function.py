d19ac4ca6f957e835056560f385fa0d0c3c4d631
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-05 11:51:12 +0530

    Program to understand the Max and Min function

---
+++ Max_function.py
@@ -0,0 +1,19 @@
+#Program to understand the "Max" and "Min" function
+
+#Function Max
+def max(n1,n2):
+	if n1 > n2:
+		return n1
+	else :
+		return n2
+
+def min(a1,a2):
+	if a1 < a2:
+		return a1
+	else:
+		return a2
+#test
+print (max(3,5))
+print (max(10,6))
+print (min(1,3))
+print (min(3,5))
