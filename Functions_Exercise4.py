d98e9142200126a5d3f707f3c36cbd0554abbf29
sudhamshua <sudhamshua@PRINHYLTPAP0887.local>
2018-09-10 12:29:04 +0530

    Program to calculate sqrt using math module

---
+++ Functions_Exercise4.py
@@ -0,0 +1,9 @@
+#Program to calculate sqrt using math module
+#imporing math for sqrt
+
+from math import sqrt
+
+#Ask the user to provide a number to find the sqrt
+number = int (input("Provide a number to find the sqrt: "))
+
+print (sqrt(number))
