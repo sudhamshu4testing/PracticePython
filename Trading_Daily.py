32b3538e500ef4312277333cf8e49ed37745cf2c
sudhamshu4testing <sudhamshu.rao@gmail.com>
2018-09-28 20:38:50 +0530

    Delete Trading_Daily.py

--- Trading_Daily.py
+++
@@ -1,9 +0,0 @@
-
-
-import requests
-from bs4 import BeautifulSoup
-raw_html = simple_get('http://finance.google.com')
-html = BeautifulSoup(html, 'html.parser')
-for i, li in enumerate(html.select('li')):
-        print(i, li.text)
-
