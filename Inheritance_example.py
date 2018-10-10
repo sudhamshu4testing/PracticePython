5fa046837d883aa58cfce5466b3ccc734bb6e0ee
sudhamshua <sudhamshua@vpn-68-90-105-116.vpn.sbcis.sbc.com>
2018-10-10 15:27:00 +0530

    Program to undersand how inheritance works

---
+++ Inheritance_example.py
@@ -0,0 +1,16 @@
+#Program to undersand how inheritance works
+
+class A:
+	def a(self):
+		print(1)
+
+class B(A):
+	def a(self):
+		print(2)
+
+class C(B):
+	def c(self):
+		print(3)
+
+c = C()
+c.a()
